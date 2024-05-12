105d52daef5aa47b23593086206dfccc8ca94162
initialencounter <2911583893@qq.com>
2024-05-12 11:18:15 +0800

    chore: more readable

--- debian.sh
+++ debian.sh
@@ -108,7 +108,43 @@ sleep $SLEEP_TIME
 cat > $sys_name-$AH.sh <<- EOM
 #!/bin/bash
 unset LD_PRELOAD
-proot --bind=/vendor --bind=/system --bind=/data/data/com.termux/files/usr --bind=/storage --bind=/storage/self/primary:/sdcard --bind=/data/data/com.termux/files/home --bind=/data/data/com.termux/cache --bind=/data/dalvik-cache --bind=$sys_name-$AH/tmp:/dev/shm --bind=$sys_name-$AH/etc/proc/vmstat:/proc/vmstat --bind=$sys_name-$AH/etc/proc/version:/proc/version --bind=$sys_name-$AH/etc/proc/uptime:/proc/uptime --bind=$sys_name-$AH/etc/proc/stat:/proc/stat --bind=$sys_name-$AH/etc/proc/loadavg:/proc/loadavg  --bind=$sys_name-$AH/etc/proc/bus/pci/00:/proc/bus/pci/00 --bind=$sys_name-$AH/etc/proc/devices:/proc/bus/devices --bind=$sys_name-$AH/etc/proc/bus/input/devices:/proc/bus/input/devices --bind=$sys_name-$AH/etc/proc/modules:/proc/modules   --bind=/sys --bind=/proc/self/fd/2:/dev/stderr --bind=/proc/self/fd/1:/dev/stdout --bind=/proc/self/fd/0:/dev/stdin --bind=/proc/self/fd:/dev/fd --bind=/proc --bind=/dev/urandom:/dev/random --bind=/data/data/com.termux/files/usr/tmp:/tmp --bind=/data/data/com.termux/files:$sys_name-$AH/termux --bind=/dev --root-id --cwd=/root -L --kernel-release=5.17.18-perf --sysvipc --link2symlink --kill-on-exit --rootfs=$sys_name-$AH/ /usr/bin/env -i HOME=/root LANG=zh_CN.UTF-8 TERM=xterm-256color /bin/su -l root
+proot \
+ --bind=/vendor \
+ --bind=/system \
+ --bind=/data/data/com.termux/files/usr \
+ --bind=/storage \
+ --bind=/storage/self/primary:/sdcard \
+ --bind=/data/data/com.termux/files/home \
+ --bind=/data/data/com.termux/cache \
+ --bind=/data/dalvik-cache \
+ --bind=$sys_name-$AH/tmp:/dev/shm \
+ --bind=$sys_name-$AH/etc/proc/vmstat:/proc/vmstat \
+ --bind=$sys_name-$AH/etc/proc/version:/proc/version \
+ --bind=$sys_name-$AH/etc/proc/uptime:/proc/uptime \
+ --bind=$sys_name-$AH/etc/proc/stat:/proc/stat \
+ --bind=$sys_name-$AH/etc/proc/loadavg:/proc/loadavg  \
+ --bind=$sys_name-$AH/etc/proc/bus/pci/00:/proc/bus/pci/00 \
+ --bind=$sys_name-$AH/etc/proc/devices:/proc/bus/devices \
+ --bind=$sys_name-$AH/etc/proc/bus/input/devices:/proc/bus/input/devices \
+ --bind=$sys_name-$AH/etc/proc/modules:/proc/modules   \
+ --bind=/sys \
+ --bind=/proc/self/fd/2:/dev/stderr \
+ --bind=/proc/self/fd/1:/dev/stdout \
+ --bind=/proc/self/fd/0:/dev/stdin \
+ --bind=/proc/self/fd:/dev/fd \
+ --bind=/proc \
+ --bind=/dev/urandom:/dev/random \
+ --bind=/data/data/com.termux/files/usr/tmp:/tmp \
+ --bind=/data/data/com.termux/files:$sys_name-$AH/termux \
+ --bind=/dev \
+ --root-id \
+ --cwd=/root \
+ -L \
+ --kernel-release=5.17.18-perf \
+ --sysvipc \
+ --link2symlink \
+ --kill-on-exit \
+ --rootfs=$sys_name-$AH/ /usr/bin/env -i HOME=/root LANG=zh_CN.UTF-8 TERM=xterm-256color /bin/su -l root
 EOM
 
 echo "授予启动脚本执行权限"
